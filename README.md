6e03c80f24e9460100eb2e11dc1d8189af2b80fa
vladmihm <89536373+vladmihm@users.noreply.github.com>
2021-09-20 13:01:39 +0100

    Upload Demo

--- README.md
+++ README.md
@@ -1,17 +1,88 @@
-## My Project
+# Alexa Entities Python Demo
 
-TODO: Fill this README out!
+This code sample shows how to use [Alexa Entities](https://developer.amazon.com/en-US/docs/alexa/custom-skills/alexa-entities-reference.html) to build a skill using Alexa's knowledge graph. This tutorial demonstrates the process using a dedicated AWS account updated with CloudFormation. Other solutions include Alexa-hosted Skills and non-AWS deployments. The skill implementation uses Python classes to create request handlers. Alternatively, you can use decorators; see [here](https://github.com/alexa-samples/skill-sample-python-helloworld-decorators) for a _Hello World_ template. 
 
-Be sure to:
+## What You Will Need
 
-* Change the title in this README
-* Edit your repository description on GitHub
+* [Amazon Developer Account](http://developer.amazon.com/alexa)
+* [ASK CLI](https://developer.amazon.com/en-US/docs/alexa/smapi/quick-start-alexa-skills-kit-command-line-interface.html)
+* [Amazon Web Services Account](http://aws.amazon.com/)
 
-## Security
+## Setting Up the Demo
 
-See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more information.
+This Python code sample includes the interaction model and skill code. Directories are structured to make it easy to deploy if you have the ASK CLI already setup (`ask configure`).
 
-## License
+1. Open a command-line terminal.
+2. Go to the directory where this `README.md` file is located.
+3. Enter `ask init` and complete the prompts as follows:
+   * For _Skill Id_ leave empty;
+   * Use default for _Skill package path_ and _Lambda code path for default region_;
+   * Type `Y` to use AWS CloudFormation to deploy Lambda;
+   * For _Lambda runtime_ use `python3.6`;
+   * For _Lambda handler_ use `entities_demo.handler`;
+   * Type `Y` to accept the configuration.
+4. Enter `ask deploy`.
 
-This library is licensed under the Amazon Software License.
+If you would like to use the [Developer Console](https://developer.amazon.com/alexa/console/ask), you can follow the steps outlined in the [Hello World](https://github.com/alexa-samples/skill-sample-python-helloworld-classes) example, substituting the [interaction model](./skill-package/interactionModels/custom/en-US.json) and the [skill code](./lambda/entities_demo.py) when called for. 
 
+## Running the Demo
+
+> User: Alexa, open knowledge demo.
+
+> Alexa: Welcome, ask me about a country.
+
+> User: Tell me something about Canada.
+
+> Alexa: Canada's political leader is Justin Trudeau. Its capital city is Ottawa.
+
+## Step-by-step Instructions
+
+This section is going to walk you through the implementation of this code sample. Consider you want to build an Alexa skill that provides information about a country.
+
+### Interaction Model
+
+First, define a new intent `CountryKnowledgeIntent` in your interaction model.
+
+```
+tell me something about {country}
+what can you tell me about {country}
+what do you know of {country}
+```
+
+Assign the `AMAZON.Country` slot type to the `country` slot. Save the model and rebuild it.
+
+### Intent Handler
+
+Next, implement an intent handler for `CountryKnowledgeIntent`. With Alexa Entities, the `AMAZON.Country` built-in slot type now returns Alexa Entity Identifiers that correspond to the matched slot value. Alexa Entity Identifiers are URLs that can be "dereferenced" to obtain knowledge about them.
+
+Check out the `CountryKnowledgeIntent` handler in `entities_demo.py` to see how it is implemented. Here's a high-level overview:
+
+1. Get the API access token from the intent request.
+2. Get the Alexa Entity Identifier associated with the `country` slot value.
+3. Send a request to the Linked Data API to fetch information associated with entity.
+4. If the request is successful, look up the value of the `politicalLeader` and `capital` properties.
+
+Here, we've used the requests library to make HTTPS requests, but any HTTPS client library can be used to call the API. Find out more about the API in the [documentation](https://developer.amazon.com/en-US/docs/alexa/custom-skills/linked-data-api-reference.html).
+
+### Skill Invocation
+
+Let's see what happens when a customer invokes our test skill. For example, they might say:
+
+> Tell me something about Canada.
+
+If "Canada" could be resolved to an entity, the slot results include an Alexa Entity Identifier that looks like this: https://ld.amazonalexa.com/entities/v1/KergWGkJmd9FT9YSvzn3AL.
+
+The intent handler processes the request, gets one of the Alexa Entity Identifier and sends an HTTPS request to the Linked Data API. The API returns a JSON-LD document containing information about the requested entity (Canada). See the API documentation for a [sample response](https://developer.amazon.com/en-US/docs/alexa/custom-skills/linked-data-api-reference.html#response-body).
+
+Finally, the intent handler builds the answer using the information obtained, prompting Alexa to respond:
+
+> Canada's political leader is Justin Trudeau. Its capital city is Ottawa.
+
+## Next Steps
+
+Now edit the code sample and build your own experience to learn more about what Alexa Entities can do. Here are a few ideas of things to try.
+
+* **Handle special cases** `politicalLeader` is not guaranteed to return for every country. Create a special response for that case and test it out using `Portugal` or `Isle of Man`.
+* **Use other properties.** Augment the response to mention the country's `humanPopulation`.
+* **Traverse the knowledge graph.** Follow entity links and make another API call to obtain the `politicalLeader`'s `birthplace`.
+* **Expand to other types.** Update the interaction model and write an intent handler to implement dialogs about `AMAZON.City` entities or other [supported built-in slot types](https://developer.amazon.com/en-US/docs/alexa/custom-skills/alexa-entities-reference.html).
---
+++ lambda/entities_demo.py
@@ -0,0 +1,213 @@
+# Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: LicenseRef-.amazon.com.-AmznSL-1.0
+# Licensed under the Amazon Software License  http://aws.amazon.com/asl/
+
+import logging
+from typing import List
+
+import ask_sdk_core.utils as ask_utils
+import requests
+from ask_sdk_core.dispatch_components import AbstractExceptionHandler, AbstractRequestHandler
+from ask_sdk_core.handler_input import HandlerInput
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_model import Response
+from ask_sdk_model.slot import Slot
+from ask_sdk_model.slu.entityresolution.resolution import Resolution
+from ask_sdk_model.slu.entityresolution.status_code import StatusCode
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class LaunchRequestHandler(AbstractRequestHandler):
+    """Handler for Skill Launch."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+
+        return ask_utils.is_request_type("LaunchRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speak_output = "Welcome, ask me about a country."
+
+        return (
+            handler_input.response_builder
+                         .speak(speak_output)
+                         .ask(speak_output)
+                         .response
+        )
+
+
+def get_slot_resolutions(slot):
+    # type: (Slot | None) -> List[Resolution] | None
+    if (slot is None or
+            slot.resolutions is None or
+            slot.resolutions.resolutions_per_authority is None):
+        return None
+
+    return [resolution for resolution in slot.resolutions.resolutions_per_authority if
+            resolution.authority == "AlexaEntities" and resolution.status.code == StatusCode.ER_SUCCESS_MATCH]
+
+
+class CountryKnowledgeIntentHandler(AbstractRequestHandler):
+    """Handler for Country Knowledge Intent."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_intent_name("CountryKnowledgeIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        api_access_token = ask_utils.get_api_access_token(handler_input)
+        slot = ask_utils.get_slot(handler_input, "country")
+        resolutions = get_slot_resolutions(slot)
+
+        if resolutions:
+            resolved_entity = resolutions[0].values[0].value.id
+            headers = {
+                "Authorization": f"Bearer {api_access_token}",
+                "Accept-Language": ask_utils.get_locale(handler_input)
+            }
+            response = requests.get(resolved_entity, headers=headers)
+
+            if response.status_code == 200:
+                entity = response.json()
+                logger.debug(entity)
+                if "capital" in entity and "politicalLeader" in entity:
+                    country = entity["name"][0]["@value"]
+                    city = entity["capital"][0]["name"][0]["@value"]
+                    person = entity["politicalLeader"][0]["name"][0]["@value"]
+                    return (
+                        handler_input.response_builder
+                                     .speak(f"{country}'s political leader is {person}. Its capital city is {city}.")
+                                     .ask("Ask me about another country.")
+                                     .response
+                    )
+
+        return (
+            handler_input.response_builder
+                         .speak("Sorry, I\'m not sure about that.")
+                         .ask("Ask me about another country")
+                         .response
+        )
+
+
+class HelpIntentHandler(AbstractRequestHandler):
+    """Handler for Help Intent."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_intent_name("AMAZON.HelpIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speak_output = "You can ask me about any country! How can I help?"
+
+        return (
+            handler_input.response_builder
+                         .speak(speak_output)
+                         .ask(speak_output)
+                         .response
+        )
+
+
+class CancelOrStopIntentHandler(AbstractRequestHandler):
+    """Single handler for Cancel and Stop Intent."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return (ask_utils.is_intent_name("AMAZON.CancelIntent")(handler_input) or
+                ask_utils.is_intent_name("AMAZON.StopIntent")(handler_input))
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speak_output = "Goodbye!"
+
+        return (
+            handler_input.response_builder
+                         .speak(speak_output)
+                         .response
+        )
+
+
+class SessionEndedRequestHandler(AbstractRequestHandler):
+    """Handler for Session End."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_request_type("SessionEndedRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+
+        # Any cleanup logic goes here.
+
+        return handler_input.response_builder.response
+
+
+class IntentReflectorHandler(AbstractRequestHandler):
+    """The intent reflector is used for interaction model testing and debugging.
+    It will simply repeat the intent the user said. You can create custom handlers
+    for your intents by defining them above, then also adding them to the request
+    handler chain below.
+    """
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_request_type("IntentRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        intent_name = ask_utils.get_intent_name(handler_input)
+        speak_output = f"You just triggered {intent_name}."
+
+        return (
+            handler_input.response_builder
+                         .speak(speak_output)
+            #            .ask("add a reprompt if you want to keep the session open for the user to respond")
+                         .response
+        )
+
+
+class CatchAllExceptionHandler(AbstractExceptionHandler):
+    """Generic error handling to capture any syntax or routing errors. If you receive an error
+    stating the request handler chain is not found, you have not implemented a handler for
+    the intent being invoked or included it in the skill builder below.
+    """
+
+    def can_handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> bool
+        return True
+
+    def handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> Response
+        logger.error(exception, exc_info=True)
+
+        speak_output = "Sorry, I had trouble doing what you asked. Please try again."
+
+        return (
+            handler_input.response_builder
+                         .speak(speak_output)
+                         .ask(speak_output)
+                         .response
+        )
+
+
+# The SkillBuilder object acts as the entry point for your skill, routing all request and response
+# payloads to the handlers above. Make sure any new handlers or interceptors you've
+# defined are included below. The order matters - they're processed top to bottom.
+
+sb = SkillBuilder()
+
+sb.add_request_handler(LaunchRequestHandler())
+sb.add_request_handler(CountryKnowledgeIntentHandler())
+sb.add_request_handler(HelpIntentHandler())
+sb.add_request_handler(CancelOrStopIntentHandler())
+sb.add_request_handler(SessionEndedRequestHandler())
+# make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+sb.add_request_handler(IntentReflectorHandler())
+
+sb.add_exception_handler(CatchAllExceptionHandler())
+
+handler = sb.lambda_handler()
